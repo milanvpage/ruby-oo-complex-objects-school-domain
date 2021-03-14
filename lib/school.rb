b06fcef450f660b2c065c89bb5aa758fa9a2d542
Milan <milanvpage@gmail.com>
2021-03-13 20:42:36 -0800

    commit tree 75059e08de829aa8f10d3a0146a896d0ec4cceb2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+attr_accessor :school, :roster
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+def add_student(student,grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+end
+
+def grade(grade)
+    @roster[grade]
+end
+
+def sort
+    student_hash = {}
+    @roster.each do |grade,students|
+        student_hash[grade] = students.sort
+    end
+    student_hash
+end
+end
